8f00dfd482f307240b7e760564d363c90780c144
Roni Sikder <ronisikder200@gmail.com>
2025-12-09 17:27:43 +0530

    2nd Commit

--- client/src/jsx-components/Template.jsx
+++ client/src/jsx-components/Template.jsx
@@ -10,10 +10,56 @@ import {
 } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { Textarea } from "@/components/ui/textarea";
+import { useRef } from "react";
+import { useMutation } from "@tanstack/react-query";
+import { toast } from "sonner";
 
-const Template = () => {
+const Template = async () => {
+  const email = useRef(null);
+  const appPass = useRef(null);
+  const mailSubject = useRef(null);
+  const mailBody = useRef(null);
+
+  const mutate = useMutation({
+    mutationFn: async (payload) => {
+      const url = "http://localhost:2000/service/user/admin/srequest";
+      const response = await fetch(url, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(payload),
+      });
+      // const responseData = await response.json().catch(() => null);
+      // if (!response.ok) {
+      //   const message = responseData?.message || "Request not sent";
+      //   throw new Error(message);
+      // }
+      // console.log("Response Data:", responseData);
+    },
+    onSuccess: toast("Template is Initiated and Test Mail Send is Done"),
+    onError: toast(Error),
+  });
+
+  const sender = async () => {
+    const user = email.current?.value ?? "";
+    const pass = appPass.current?.value ?? "";
+    const sub = mailSubject.current?.value ?? "";
+    const body = mailBody.current?.value ?? "";
+    if (!user || !pass || !sub || !body) {
+      throw new Error("All Fields are Require to SetUp the Template");
+    }
+    const payload = {
+      sender: user,
+      appPassword: pass,
+      subject: sub,
+      mailBody: body,
+    };
+    mutate.mutate(payload);
+  };
+
   return (
-    <div className="template border-2 border-black rounded">
+    <div className="template border-2 border-black rounded w-4/8">
       <Card className="rounded-[1px]">
         <CardHeader>
           <CardTitle className="text-center">Template Creation</CardTitle>
@@ -25,20 +71,29 @@ const Template = () => {
           <div className="gap-y-10">
             <div className="mb-2">
               <label htmlFor="senderEmail">Sender Email Address</label>
-              <Input id="senderEmail" placeholder="Email" />
+              <Input id="senderEmail" ref={email} placeholder="Email" />
             </div>
             <div className="mb-2">
               <label htmlFor="appPassword">App Password</label>
-              <Input id="appPassword" placeholder="App Password" />
+              <Input
+                id="appPassword"
+                ref={appPass}
+                placeholder="App Password"
+              />
             </div>
             <div className="mb-2">
               <label htmlFor="mailSubject">Mail Subject</label>
-              <Input id="mailSubject" placeholder="Mail Subject" />
+              <Input
+                id="mailSubject"
+                ref={mailSubject}
+                placeholder="Mail Subject"
+              />
             </div>
             <div>
               <label htmlFor="mailbody-template">Mail Body Template</label>
               <Textarea
                 id="mailbody-template"
+                ref={mailBody}
                 placeholder="Enter your mail body template here..."
                 className="mb-1"
               />
@@ -52,6 +107,7 @@ const Template = () => {
           <Button
             className="bg-green-800 text-white rounded w-full"
             variant="outline"
+            onClick={() => sender()}
           >
             Create Template
           </Button>
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,9 +3,15 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import { Toaster } from "@/components/ui/sonner";
 
+const queryClient = new QueryClient();
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <QueryClientProvider client={queryClient}>
+    <BrowserRouter>
+      <App />
+      <Toaster />
+    </BrowserRouter>
+  </QueryClientProvider>,
 );
