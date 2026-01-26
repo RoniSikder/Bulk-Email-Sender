be7da735d367af69b6bdff1d653c899a721b9172
Roni Sikder <ronisikder200@gmail.com>
2026-01-27 01:11:00 +0530

    some update

--- client/src/jsx-components/Upload.jsx
+++ client/src/jsx-components/Upload.jsx
@@ -9,13 +9,15 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
-import { useLocation, useNavigate } from "react-router";
+import { Navigate, useLocation, useNavigate } from "react-router";
 import * as XLSX from "xlsx";
+import { useRef } from "react";
+
 
 const Upload = () => {
   let location = useLocation();
   const data = location.state;
-  console.log(data);
+  const Navigate = useNavigate();
   const createdemoxlsx = (subcol, bodycol) => {
     const wb = XLSX.utils.book_new();
     let sub = [];
@@ -26,10 +28,26 @@ const Upload = () => {
     for (let i = 0; i < bodycol; i++) {
       body.push(`body${i + 1}`);
     }
-    const ws = XLSX.utils.aoa_to_sheet([sub, body]);
+    let data_array = ["Recipient_Email",...sub, ...body];
+    const ws = XLSX.utils.aoa_to_sheet([data_array]);
     XLSX.utils.book_append_sheet(wb, ws, "Sheet1");
     XLSX.writeFile(wb, "demo.xlsx");
   };
+  
+  const uploadData = useRef(null);
+  const handlefileUpload = ()=>{
+    const reader = new FileReader();
+    console.log(uploadData.current.files[0]);
+    reader.onload = (e) => {
+      const data = new Uint8Array(e.target.result);
+      const workbook = XLSX.read(data, { type: "array" });
+      const jsonData = XLSX.utils.sheet_to_json(workbook.Sheets.Sheet1);
+      console.log(jsonData[0]);
+      Navigate("/preview", { state: jsonData });
+    };
+    reader.readAsArrayBuffer(uploadData.current.files[0]);
+  }
+
   return (
     <div className="template border-2 border-black rounded w-4/8">
       <Card className="rounded-[1px] h-full">
@@ -45,6 +63,7 @@ const Upload = () => {
               <Input
                 placeholder="Upload File"
                 type="file"
+                ref={uploadData}
                 accept=".csv,.xlsx,.xls"
                 className=""
                 id="upload_excel"
@@ -66,7 +85,7 @@ const Upload = () => {
             <Button className="bg-red-800 text-white rounded-[3px]">
               Previous
             </Button>
-            <Button className="bg-green-800 text-white rounded-[3px]">
+            <Button onClick={handlefileUpload} className="bg-green-800 text-white rounded-[3px]">
               Next
             </Button>
           </div>
